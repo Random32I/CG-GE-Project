146bd8cbc7467549f850a28deae7bb6252467aa4
Icey-4 <isis.powers@ontariotechu.net>
2024-10-29 12:56:36 -0400

    LUT stuff

--- Assets/Scripts/ColourGrade.cs
+++ Assets/Scripts/ColourGrade.cs
@@ -1,12 +1,23 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 public class ColourGrade : MonoBehaviour
 {
     //public Shader awesomeShader = null;
-    public Material m_renderMaterial;
+    public Material[] whichLut = new Material[4];
+    public int x = 0;
     void OnRenderImage(RenderTexture source, RenderTexture destination)
     {
-        Graphics.Blit(source, destination, m_renderMaterial);
+        Graphics.Blit(source, destination, whichLut[x]);
+    }
+
+    public void SwitchLut()
+    {
+        if (x == 3)
+        {
+            x = 0;
+        }
+        else
+        {
+            x++;
+        }
     }
 }
